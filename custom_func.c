73d30c69e011a58ff530de1d43eb75b3c4ec6960
Rafig Mammadov <131705272+rafigmammadov@users.noreply.github.com>
2024-05-04 01:44:38 +0400

    Update custom_func.c

--- custom_func.c
+++ custom_func.c
@@ -1,33 +1,53 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-/* custom_strdup: Custom implementation of strdup */
+/* Custom implementation of strdup */
 char *custom_strdup(const char *src)
 {
-    if (src == NULL)return NULL;
-    size_t len = 0;
-    while (src[len] != '\0') len++;
-    char *dst = malloc(len + 1);
-    if (dst == NULL) return NULL;
-    for (size_t i = 0; i <= len; i++) dst[i] = src[i];
-    return dst;
+    size_t len;
+    size_t i;
+    
+    if (src == NULL)
+        return (NULL);
+    
+    len = 0;
+    while (src[len] != '\0')
+    {
+        len++;
+    }
+    
+    char *dst = (char *)malloc((len + 1) * sizeof(char));
+    if (dst == NULL)
+        return (NULL);
+    
+    for (i = 0; i <= len; i++)
+    {
+        dst[i] = src[i];
+    }
+    
+    return (dst);
 }
 
-/* custom_strcmp: Custom implementation of strcmp */
+/* Custom implementation of strcmp */
 int custom_strcmp(const char *s1, const char *s2)
 {
-    while (*s1 && *s2 && *s1 == *s2)
-    {
+    while (*s1 && *s2 && *s1 == *s2) {
         s1++;
         s2++;
     }
-    return (unsigned char)(*s1) - (unsigned char)(*s2);
+    return ((unsigned char)(*s1) - (unsigned char)(*s2));
 }
 
-/* custom_strlen: Custom implementation of strlen */
+/* Custom implementation of strlen */
 size_t custom_strlen(const char *s)
 {
-    size_t len = 0;
-    while (s[len] != '\0') len++;
-    return len;
+    size_t len;
+
+    len = 0;
+
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+    return (len);
 }
