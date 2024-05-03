833cf41a6d7a78010debbc9ac0ae8befde04712f
Rafig Mammadov <131705272+rafigmammadov@users.noreply.github.com>
2024-05-04 00:32:10 +0400

    The function custom_getline has been written

--- getcommand.c
+++ getcommand.c
@@ -1,5 +1,54 @@
 #include <errno.h>
 #include "main.h"
+
+#define BUFFER_SIZE 1024
+
+/**
+  * custom_getline - Custom getline function
+  * Return: Returns string from buffer
+  */
+
+char *custom_getline(void)
+{
+	static char buffer[BUFFER_SIZE];
+	static int pos = 0;
+	static int bytes_read = 0;
+	char *line = NULL;
+	int line_size = 0;
+
+	while (1)
+	{
+		if (pos >= bytes_read)
+		{
+			bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+			pos = 0;
+			if (bytes_read <= 0)
+				return (NULL);
+		}
+
+		while (pos < bytes_read)
+		{
+			if (buffer[pos] == '\n')
+			{
+				line = (char *)malloc(line_size + 1);
+				if (line == NULL)
+					return (NULL);
+				
+				int i;
+				
+				for (i = 0; i < line_size; i++)
+					line[i] = buffer[pos - line_size + i];
+				line[i] = '\0';
+				pos++;
+				return (line);
+			}
+			line_size++;
+			pos++;
+		}
+	}
+}
+
+
 /**
   * arg_counter - counts number of arguments
   * @buf: user input
@@ -27,7 +76,7 @@ char **get_command(char **buf)
 	char **array;
 	size_t n = 1, k, i, l = 0, count;
 
-	*buf = malloc(n), k = getline(buf, &n, stdin);
+	*buf = custom_getline();
 	if (k == (size_t)(-1))
 	{
 		if (errno == 0)
